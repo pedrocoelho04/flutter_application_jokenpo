1629e8b9e9a46d7fdaad9df0f5b1077426f18c72
pedrocoelho04 <bunnybrazilian@gmail.com>
2026-04-15 21:47:05 -0300

    Update: Refatorado estrutura do codigo

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatefulWidget{
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
@@ -13,58 +13,72 @@ class MainApp extends StatefulWidget{
 }
 
 class _MainAppState extends State<MainApp> {
-
   String? op_player;
   String? op_sistema;
   List<String> opcoes = ["Pedra", "Papel", "Tesoura"];
 
-
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("AP 1 - Jokenpo"),
+          title: Text(
+            "AP 1 - Jokenpo",
+            style: TextStyle(fontSize: 18, color: Colors.white),
+          ),
           backgroundColor: Color.fromARGB(255, 28, 0, 187),
         ),
-        body: Row(
-          children: [
-            Center(child: Text("Escolha uma opção")),
-            Column(
-              children: [
-                ElevatedButton(
-                  onPressed: null, 
-                  child: Icon(FontAwesomeIcons.hand.data)
-                  ),
-                ElevatedButton(
-                  onPressed: null, 
-                  child: Icon(FontAwesomeIcons.handFist.data)
-                  ),
-                ElevatedButton(
-                  onPressed: null, 
-                  child: Icon(FontAwesomeIcons.handScissors.data)
-                  ),
-              ],
-            ),
-            Column(
-              children: [
-                ElevatedButton(
-                  onPressed: null, 
-                  child: Icon(FontAwesomeIcons.hand.data)
-                  ),
-                ElevatedButton(
-                  onPressed: null, 
-                  child: Icon(FontAwesomeIcons.handFist.data)
-                  ),
-                ElevatedButton(
-                  onPressed: null, 
-                  child: Icon(FontAwesomeIcons.handScissors.data)
-                  ),
-              ],
-            ),
-            TextButton(onPressed: null, child: Container(),)
-          ],
+        body: Center(
+          child: Padding(padding: EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Escolha uma opção"),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: null,
+                      child: Icon(FontAwesomeIcons.hand.data),
+                    ),
+                    ElevatedButton(
+                      onPressed: null,
+                      child: Icon(FontAwesomeIcons.handFist.data),
+                    ),
+                    ElevatedButton(
+                      onPressed: null,
+                      child: Icon(FontAwesomeIcons.handScissors.data),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: null,
+                      child: Icon(FontAwesomeIcons.hand.data),
+                    ),
+                    ElevatedButton(
+                      onPressed: null,
+                      child: Icon(FontAwesomeIcons.handFist.data),
+                    ),
+                    ElevatedButton(
+                      onPressed: null,
+                      child: Icon(FontAwesomeIcons.handScissors.data),
+                    ),
+                  ],
+                ),
+              ),
+              TextButton(onPressed: null, child: Container()),
+            ],
+          ),
+          ),
         ),
       ),
     );
