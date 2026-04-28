ea5c8b9a99fccbbe2a8cf1d18ad2e73615e0135c
pedrocoelho04 <bunnybrazilian@gmail.com>
2026-04-28 17:53:03 -0300

    feat: implementado versão final da UI da tela e finalizado logica do jogo

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -13,9 +14,45 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  String? op_player;
-  String? op_sistema;
+  String? opPlayer;
+  String? opSistema;
+  String? resultado;
   List<String> opcoes = ["Pedra", "Papel", "Tesoura"];
+
+  void jogar(String escolhaPlayer) {
+    final random = Random();
+    final escolhaSistema = opcoes[random.nextInt(3)];
+
+    String res;
+    if (escolhaPlayer == escolhaSistema) {
+      res = "Empate!";
+    } else if ((escolhaPlayer == "Pedra" && escolhaSistema == "Tesoura") ||
+        (escolhaPlayer == "Papel" && escolhaSistema == "Pedra") ||
+        (escolhaPlayer == "Tesoura" && escolhaSistema == "Papel")) {
+      res = "Você ganhou!";
+    } else {
+      res = "Você perdeu!";
+    }
+
+    setState(() {
+      opPlayer = escolhaPlayer;
+      opSistema = escolhaSistema;
+      resultado = res;
+    });
+  }
+
+  IconData getIcone(String opcao) {
+    switch (opcao) {
+      case "Pedra":
+        return FontAwesomeIcons.handFist as IconData;
+      case "Papel":
+        return FontAwesomeIcons.hand as IconData;
+      case "Tesoura":
+        return FontAwesomeIcons.handScissors as IconData;
+      default:
+        return FontAwesomeIcons.question as IconData;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,54 +67,99 @@ class _MainAppState extends State<MainApp> {
           backgroundColor: Color.fromARGB(255, 28, 0, 187),
         ),
         body: Center(
-          child: Padding(padding: EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Escolha uma opção"),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Escolha uma opção:",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 16),
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    ElevatedButton(
-                      onPressed: null,
-                      child: Icon(FontAwesomeIcons.hand.data),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: ElevatedButton(
+                        onPressed: () => jogar("Pedra"),
+                        child: Icon(getIcone("Pedra")),
+                      ),
                     ),
-                    ElevatedButton(
-                      onPressed: null,
-                      child: Icon(FontAwesomeIcons.handFist.data),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: ElevatedButton(
+                        onPressed: () => jogar("Papel"),
+                        child: Icon(getIcone("Papel")),
+                      ),
                     ),
-                    ElevatedButton(
-                      onPressed: null,
-                      child: Icon(FontAwesomeIcons.handScissors.data),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: ElevatedButton(
+                        onPressed: () => jogar("Tesoura"),
+                        child: Icon(getIcone("Tesoura")),
+                      ),
                     ),
                   ],
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: null,
-                      child: Icon(FontAwesomeIcons.hand.data),
+                SizedBox(height: 32),
+                if (opPlayer != null &&
+                    opSistema != null &&
+                    resultado != null) ...[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            "Você",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 8),
+                          Icon(getIcone(opPlayer!), size: 40),
+                          SizedBox(height: 4),
+                          Text(opPlayer!),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 24),
+                        child: Text(
+                          "VS",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            "Celular",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 8),
+                          Icon(getIcone(opSistema!), size: 40),
+                          SizedBox(height: 4),
+                          Text(opSistema!),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 24),
+                  Text(
+                    resultado!,
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: resultado == "Você ganhou!" ? Colors.green
+                           : resultado == "Você perdeu!" ? Colors.red
+                           : Colors.orange,
                     ),
-                    ElevatedButton(
-                      onPressed: null,
-                      child: Icon(FontAwesomeIcons.handFist.data),
-                    ),
-                    ElevatedButton(
-                      onPressed: null,
-                      child: Icon(FontAwesomeIcons.handScissors.data),
-                    ),
-                  ],
-                ),
-              ),
-              TextButton(onPressed: null, child: Container()),
-            ],
-          ),
+                  ),
+                ],
+              ],
+            ),
           ),
         ),
       ),
